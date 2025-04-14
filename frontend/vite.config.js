887d0c64334856c0d7fd7a77c6e6d4cfe56458f0
boiledsteak <timothymxa@gmail.com>
2025-04-14 15:24:21 +0800

    Update file vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../backend/build',
+    outDir: 'dist',
     emptyOutDir: true
   },
   resolve: {
