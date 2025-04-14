8273ef4c3a354f3e6257f0ef19cd1f3008a4d6b1
boiledsteak <timothymxa@gmail.com>
2025-04-14 18:53:54 +0800

    Update file server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const __dirname = dirname(__filename);
 
 // Serve static assets from the frontend build folder
 // This assumes the React app has been built (npm run build)
-app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+app.use(express.static(path.join(__dirname, '../frontend', 'dist')));
 
 // Handle API routes or any other server-side logic
 app.get("/api/*name", (req, res) => {
@@ -22,7 +22,7 @@ app.get("/api/*name", (req, res) => {
 
 // Serve the React app for any route that doesn't match an API route
 app.get("*something", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 });
 
 // Start the server
