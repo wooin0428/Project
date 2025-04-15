8fc8ceb29603efa8e7f54e46a6f4d9284d5d2ac7
Haileyyyy <wooin0428@gmail.com>
2025-04-15 16:34:55 +0000

    Update file server.js

--- backend/server.js
+++ backend/server.js
@@ -150,7 +150,7 @@ app.get("/api/matchhistory1", async (req, res) => {
 
 // Get all cleaners shortlisted by a specific homeowner
 app.get("/api/shortlist2001", async (req, res) => {
-  const { homeowner_id} = 2001;
+  const  homeowner_id = 2001;
 
   try {
     const result = await sql`
