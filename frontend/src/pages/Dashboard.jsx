2be02aa4f0af1121276b910843a4770623dbf404
boiledsteak <timothymxa@gmail.com>
2025-04-16 15:37:32 +0800

    Update 2 files
    - /frontend/src/helpers/getUserGroup.jsx
    - /frontend/src/pages/Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,24 +1,22 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { getUserGroup } from "../helpers/getUserGroup";
 
+const Dashboard = () => {
+  const [userGroup, setUserGroup] = useState("");
 
+  useEffect(() => {
+    const fetchGroup = async () => {
+      const group = await getUserGroup();
+      setUserGroup(group);
+    };
+    fetchGroup();
+  }, []);
 
-const Login = () => {
-	return (
-		<div id="b1" style={{
-			backgroundColor: '#efeed8'
-		}}>
-
-		<h1>
-            Hello you are logged in. your user group is: 
-        </h1>
-
-		
-
-			
-		
-		</div>
-	);
+  return (
+    <div id="b1" style={{ backgroundColor: '#efeed8' }}>
+      <h1>Hello, you are logged in. Your user group is: {userGroup}</h1>
+    </div>
+  );
 };
 
-export default Login;
+export default Dashboard;
