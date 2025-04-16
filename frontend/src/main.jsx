d838d11a96c93c66b59f53d35e5c663ea98cf199
boiledsteak <timothymxa@gmail.com>
2025-04-16 13:36:45 +0800

    Update file main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./styles/main.css";
-import "./styles/hosearch.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
