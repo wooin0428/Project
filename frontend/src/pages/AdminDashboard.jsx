d45d70183bcbe658c50283be65110db47cee4f69
mahsunon <83772240+boiledsteak@users.noreply.github.com>
2025-04-18 00:22:01 +0800

    Revert "Update file AdminDashboard.jsx"
    This reverts commit 5a3fef7e16c76590d7f2966e854ab277ac050631.

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,35 +1,30 @@
 import React, { useEffect, useState } from "react";
-import UserSession from "../classes/UserSession"; // Import the UserSession class
+import { getUserGroup } from "../helpers/getUserGroup";
+import { getUsername } from "../helpers/getUsername";
 
-const HoDashboard = () => {
-  const [userSession, setUserSession] = useState(null);
+const AdminDashboard = () => {
+  const [userGroup, setUserGroup] = useState("");
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
-    // Assuming you have the user session stored somewhere, like in localStorage, or from your current app state
-    const storedUserSession = JSON.parse(localStorage.getItem("userSession"));
-
-    if (storedUserSession) {
-      const user = new UserSession(storedUserSession); // Create the UserSession instance
-      setUserSession(user); // Store the user session instance in state
-    }
+    const fetchData = async () => {
+      const group = await getUserGroup();
+      const name = await getUsername();
+      setUserGroup(group);
+      setUsername(name);
+    };
+    fetchData();
   }, []);
-
-  if (!userSession) {
-    return <div>Loading...</div>; // Or redirect to login page if session is not found
-  }
 
   return (
     <div id="b1" style={{ backgroundColor: '#efeed8' }}>
-      <h1>Hello, {userSession.username} — you are at {userSession.group} dashboard</h1>
+      <h1>Hello, {username} — you are at {userGroup} dashboard</h1>
       <div className="cleanerListingCon">
-        <div className="cleanerListingBox">
-          <h2> ADMIN PAGE!! </h2>
-          
-          
-        </div>
+       
+
       </div>
     </div>
   );
 };
 
-export default HoDashboard;
+export default AdminDashboard;
